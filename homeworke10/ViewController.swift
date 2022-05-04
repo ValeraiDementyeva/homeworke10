7502de2af557c9d36bdd17f7256a075735b53fff
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2022-05-04 23:17:44 +0300

    Initial Commit

---
+++ homeworke10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeworke10
+//
+//  Created by Валерия Дементьева on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
